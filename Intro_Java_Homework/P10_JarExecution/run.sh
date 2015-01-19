9830ad84cb968f18f792d45bd1ee906f7d74114f
AsenT <a.h.temelkov@gmail.com>
2015-01-19 20:41:33 +0200

    deleted

--- Intro_Java_Homework/P10_JarExecution/run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar /home/asen/workspace/Intro_Java_Homework/P10_JarExecution/CardsPdf.jar
--- Intro_Java_Homework/src/P3_PrintYourHometown.java
+++
@@ -1,7 +0,0 @@
-
-public class P3_PrintYourHometown {
-	public static void main (String[] args) {
-		System.out.println("Sofia");
-	}
-
-}
